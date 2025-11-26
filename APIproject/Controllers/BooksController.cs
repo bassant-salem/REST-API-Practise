b6720fcb4af28a93241b2ece2a46d96d302ee1f8
Bassant Salem <lyleee276@gmail.com>
2025-11-26 21:17:15 +0200

    basic CRUD operations

--- APIproject/Controllers/BooksController.cs
+++ APIproject/Controllers/BooksController.cs
@@ -1,6 +1,8 @@
-﻿using APIproject.Models;
+﻿using APIproject.Data;
+using APIproject.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace APIproject.Controllers
 {
@@ -8,55 +10,59 @@ namespace APIproject.Controllers
     [ApiController]
     public class BooksController : ControllerBase
     {
-        static private List<Book> books = new List<Book>
+        //static private List<Book> books = new List<Book>
+        //{
+        //    new Book
+        //    {
+        //        Id = 1,
+        //        Title = "The Metamorphosis",
+        //        Author = "Franz Kafka",
+        //        YearPublished = 1960
+        //    },
+        //    new Book
+        //    {
+        //        Id = 2,
+        //        Title = "1984",
+        //        Author = "George Orwell",
+        //        YearPublished = 1949
+        //    },
+        //    new Book
+        //    {
+        //        Id = 3,
+        //        Title = "The Brothers Karamazov",
+        //        Author = "Fyodor Dostoevsky",
+        //        YearPublished = 1880
+        //    },
+        //    new Book
+        //    {
+        //        Id = 4,
+        //        Title = "Being and Nothingness",
+        //        Author = "Jean-Paul Sartre",
+        //        YearPublished = 1943
+        //    },
+        //    new Book
+        //    {
+        //        Id = 5,
+        //        Title = "The Trial",
+        //        Author = "Franz Kafka",
+        //        YearPublished = 1925
+        //    }
+        //};
+        private readonly APIprojectContext _context;
+        public BooksController(APIprojectContext context)
         {
-            new Book
-            {
-                Id = 1,
-                Title = "The Metamorphosis",
-                Author = "Franz Kafka",
-                YearPublished = 1960
-            },
-            new Book
-            {
-                Id = 2,
-                Title = "1984",
-                Author = "George Orwell",
-                YearPublished = 1949
-            },
-            new Book
-            {
-                Id = 3,
-                Title = "The Brothers Karamazov",
-                Author = "Fyodor Dostoevsky",
-                YearPublished = 1880
-            },
-            new Book
-            {
-                Id = 4,
-                Title = "Being and Nothingness",
-                Author = "Jean-Paul Sartre",
-                YearPublished = 1943
-            },
-            new Book
-            {
-                Id = 5,
-                Title = "The Trial",
-                Author = "Franz Kafka",
-                YearPublished = 1925
-            }
-        };
-
+            _context = context;
+        }
         [HttpGet]
-        public ActionResult<List<Book>> GetAllBooks()
+        public async Task<ActionResult<List<Book>>> GetAllBooks()
         {
-            return Ok(books);
+            return Ok(await _context.Books.ToListAsync());
         }
         // The user requesting one specific resource   
         [HttpGet("{id}")]
-        public ActionResult<Book> GetBookById(int id)
+        public async  Task<ActionResult<Book>> GetBookById(int id)
         {
-            var book = books.FirstOrDefault(b => b.Id == id);
+            var book = await _context.Books.FindAsync(id);
             if (book == null)
             {
                 return NotFound();
@@ -64,33 +70,24 @@ namespace APIproject.Controllers
             return Ok(book);
         }
         [HttpPost]
-        public ActionResult<Book> AddBook(Book newBook)
+        public async Task <ActionResult<Book>> AddBook(Book newBook)
         {
-            if(newBook == null)
+            if (newBook == null)
             {
                 return BadRequest();
             }
-            // Check if book with same title and author already exists
-            var isDuplicate = books.Any(b =>
-                b.Title == newBook.Title &&
-                b.Author == newBook.Author);
-
-            if (isDuplicate)
-            {
-                return BadRequest("A book with this title and author already exists.");
-            }
-            // Auto-generate ID if not provided
-            newBook.Id = books.Count > 0 ? books.Max(b => b.Id) + 1 : 1;
-            books.Add(newBook);
+            
+            _context.Books.Add(newBook);
+            await _context.SaveChangesAsync();
             return CreatedAtAction(nameof(GetBookById), new { id = newBook.Id }, newBook);
 
         }
         [HttpPut("{id}")]
         // We used here IActionResult because we are not returning any specific type or object
         // so the most appropriate return type is status code
-        public IActionResult UpdateBook(int id,Book updatedBook)
+        public async Task<IActionResult> UpdateBook(int id, Book updatedBook)
         {
-            var book = books.FirstOrDefault(b => b.Id == id);
+            var book = await _context.Books.FindAsync(id);
             if (book == null)
             {
                 return NotFound();
@@ -100,17 +97,19 @@ namespace APIproject.Controllers
             book.Author = updatedBook.Author;
             book.YearPublished = updatedBook.YearPublished;
 
+            await _context.SaveChangesAsync();
             return NoContent();
         }
         [HttpDelete("{id}")]
-        public IActionResult DeleteBook(int id)
+        public async Task<IActionResult> DeleteBook(int id)
         {
-            var book = books.FirstOrDefault(b => b.Id == id);
+            var book = await _context.Books.FindAsync(id);
             if (book == null)
             {
                 return NotFound();
             }
-            books.Remove(book);
+            _context.Books.Remove(book);
+            await _context.SaveChangesAsync();
             return NoContent();
         }
 
